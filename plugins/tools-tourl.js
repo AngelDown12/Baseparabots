9d14f6f9b4e10a57569d3e7d7c49948358c50f1f
AngelDown12 <angeldown12xz@gmail.com>
2025-08-01 21:36:26 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -1,52 +1,64 @@
-import fs from "fs"
-import fetch from "node-fetch"
-import FormData from "form-data"
+import fetch from "node-fetch";
+import crypto from "crypto";
+import { FormData, Blob } from "formdata-node";
+import { fileTypeFromBuffer } from "file-type";
 
-let handler = async m => {
+let handler = async (m, { conn }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) return conn.reply(m.chat, `${emoji} Por favor, responde a un archivo válido (imagen, video, etc.).`, m);
+
+  await m.react(rwait);
+
   try {
-    const q = m.quoted || m
-    const mime = q.mediaType || ""    
-    if (!/image|video|audio|sticker|document/.test(mime)) 
-      throw "```[ 📤 ] Responde a una imagen / vídeo / audio ( normal o documento )```"
-    const media = await q.download(true)
-    const fileSizeInBytes = fs.statSync(media).size    
-    if (fileSizeInBytes === 0) {
-      await m.reply("```[ ❗ ] El archivo es demasiado ligero```")
-      await fs.promises.unlink(media)
-      return
-    }   
-    if (fileSizeInBytes > 1073741824) {
-      await m.reply("```[ 📌 ] El archivo supera 1GB```")
-      await fs.promises.unlink(media)
-      return
-    }    
-    const { files } = await uploadUguu(media)
-    const caption = `\`\`\`[ ⭐ ] Aquí tienes la URL de tu archivo:\n${files[0]?.url}\`\`\``
-    await m.reply(caption)
-  } catch (e) {
-    await m.reply(`${e}`)
+    let media = await q.download();
+    let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+    let link = await catbox(media);
+
+    let txt = `*乂 C A T B O X - U P L O A D E R 乂*\n\n`;
+    txt += `*» Enlace* : ${link}\n`;
+    txt += `*» Tamaño* : ${formatBytes(media.length)}\n`;
+    txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`;
+    txt += `> *${dev}*`;
+
+    await conn.sendFile(m.chat, media, 'thumbnail.jpg', txt, m, fkontak);
+
+    await m.react(done);
+  } catch {
+    await m.react(error);
   }
+};
+
+handler.help = ['tourl2'];
+handler.tags = ['transformador'];
+handler.command = ['catbox', 'tourl2'];
+export default handler;
+
+function formatBytes(bytes) {
+  if (bytes === 0) {
+    return '0 B';
+  }
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
 }
 
-handler.help = ["tourl2", "tourl"]
-handler.tags = ["tools"]
-handler.command = /^(tourl2|tourl)$/i
-export default handler
+async function catbox(content) {
+  const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
+  const blob = new Blob([content.toArrayBuffer()], { type: mime });
+  const formData = new FormData();
+  const randomBytes = crypto.randomBytes(5).toString("hex");
+  formData.append("reqtype", "fileupload");
+  formData.append("fileToUpload", blob, randomBytes + "." + ext);
 
-async function uploadUguu(path) {
-  try {
-    const form = new FormData()
-    form.append("files[]", fs.createReadStream(path))   
-    const res = await fetch("https://uguu.se/upload.php", {
-      method: "POST",
-      headers: form.getHeaders(),
-      body: form
-    })    
-    const json = await res.json()
-    await fs.promises.unlink(path)   
-    return json
-  } catch (e) {
-    await fs.promises.unlink(path)
-    throw "Upload failed"
-  }
+  const response = await fetch("https://catbox.moe/user/api.php", {
+    method: "POST",
+    body: formData,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36",
+    },
+  });
+
+  return await response.text();
 }
