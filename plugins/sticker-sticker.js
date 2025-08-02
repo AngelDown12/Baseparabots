0a10ad2e4c89a9476cc8b8158b538f785d74ae7e
AngelDown12 <angeldown12xz@gmail.com>
2025-08-01 22:52:43 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -2,19 +2,22 @@ import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
   let username = conn.getName(m.sender)
+
   try {
-  	
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
+
     if (/webp|image|video/g.test(mime)) {
       let img = await q.download?.()
-      if (!img) return m.reply(`☁️ Responde a una *Imagen* o *Vídeo.*`)
+      if (!img) return m.reply(`☁️ 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝘼𝙇 𝙑𝙄𝘿𝙀𝙊 𝙋𝘼𝙍𝘼 𝘾𝙍𝙀𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍`)
       let out
+
       try {
-        stiker = await sticker(img, false, global.packname, global.author)
+        stiker = await sticker(img, false, '', '') // sin texto abajo
       } catch (e) {
         console.error(e)
       } finally {
@@ -23,24 +26,26 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
           else if (/image/g.test(mime)) out = await uploadImage(img)
           else if (/video/g.test(mime)) out = await uploadFile(img)
           if (typeof out !== 'string') out = await uploadImage(img)
-          stiker = await sticker(false, out, global.packname, global.author)
+
+          stiker = await sticker(false, out, '', '') // también sin texto abajo
         }
       }
     } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('La *Url* es invalida')
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], '', '') // sin texto también
+      else return m.reply('La *Url* es inválida.')
     }
   } catch (e) {
     console.error(e)
     if (!stiker) stiker = e
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    else return m.reply('☁️ Responde a una *Imagen* o *Vídeo.*')
+    else return m.reply('☁️ 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼𝙇𝘼 𝙄𝙈𝘼𝙂𝙀𝙉 𝙊 𝘼𝙇 𝙑𝙄𝘿𝙀𝙊 𝙋𝘼𝙍𝘼 𝘾𝙍𝙀𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍')
   }
 }
+
 handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = ['s', 'sticker', 'stiker'] 
+handler.command = ['s', 'sticker', 'stiker']
 
 export default handler
 
